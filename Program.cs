6613a51df557869125bdc8646209a495fb53431c
antjamespadgett <padgettanthonyj@gmail.com>
2021-11-04 18:59:25 -0500

    praticed If statements

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace IfStatementsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isTall = true;
+            bool isMale = false;
+
+            if (isTall && isMale)
+            {
+                Console.WriteLine("You are tall and male");
+            }
+            else if (isTall && !isMale)
+            {
+                Console.WriteLine("Is tall and not male");
+            }
+            else
+            {
+                Console.WriteLine("You are not tall");
+            }
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+        }
+    }
+}
